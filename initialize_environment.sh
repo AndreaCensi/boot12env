11c3ffc4d2a9e8bd70cb4fceb0bdbc4ecbc3dff2
Andrea Censi <andrea@cds.caltech.edu>
2011-07-03 21:58:26 -0700

    updated initialization script

--- initialize_environment.sh
+++ initialize_environment.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -e
+set -x
 config=environment.sh
 tag=B11
 
@@ -18,11 +20,16 @@ cat ${config}
 echo "Now creating virtual environment (press enter)"
 read
 echo Create a new virtual env for python
-#virtualenv --prompt=${tag} `pwd`/deploy
-virtualenv `pwd`/deploy
+virtualenv --prompt=${tag} `pwd`/deploy
+#virtualenv `pwd`/deploy
 echo source $\{${tag}_PREFIX\}/bin/activate >> ${config}
 source ${config}
 
+echo "Now installing a few python packages (press enter)"
+read
+
+pip install --upgrade ipython
+pip install --upgrade nose
 
 #echo "Install patience"
 #pip install -e "git+git@github.com:AndreaCensi/patience.git#egg=Patience"
