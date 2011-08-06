f78a330a1dcbc944c451e0de76625b00f3cf9e8e
Andrea Censi <andrea@cds.caltech.edu>
2011-08-06 09:36:34 -0700

    minor

--- environment-susan.local.sh
+++ environment-susan.local.sh
@@ -5,7 +5,7 @@
 
 alias boot_gui_osx='/usr/bin/pythonw =boot_gui'
 
-source ~/bin/ros_install_env.sh 
+# source ~/bin/ros_install_env.sh 
 export ROS_PACKAGE_PATH=${B11_SRC}/bootstrapping_olympics/ros-packages:${ROS_PACKAGE_PATH}
 
 # Make sure we can access local packages even though we use another python
