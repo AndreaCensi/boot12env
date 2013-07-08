227b3b878516867572146e0b874392984fd1f969
Andrea Censi <andrea@cds.caltech.edu>
2013-07-08 17:27:03 +0200

    done

--- data/bo_app1/src/bo_app1/go.py
+++ data/bo_app1/src/bo_app1/go.py
@@ -3,13 +3,12 @@ from bootstrapping_olympics import logger
 from bootstrapping_olympics.programs.manager.experiments_utils import (
     experiment_explore_learn_main)
 from bootstrapping_olympics.utils import wrap_script_entry_point
-from vehicles  import VehiclesConfig
 from os.path import dirname
 import os
 
 # robots = ['r_cam360', 'r_rf360', 'r_cam360s15', 'r_sf12', 'r_sf144r']
-#robots = ['r_cam_A', 'r_rf_A', 'r_fs_A']
-robots = ['r_cam_A', 'r_rf_A', 'r_fs_A', 
+# robots = ['r_cam_A', 'r_rf_A', 'r_fs_A']
+robots = ['r_cam_A', 'r_rf_A', 'r_fs_A',
             'r_rf_A_n1', 'r_rf_A_n2',
             'r_cam_A_n1', 'r_cam_A_n2']
 agents = ['embed', 'BDSAgent_C1', 'EstStats']
